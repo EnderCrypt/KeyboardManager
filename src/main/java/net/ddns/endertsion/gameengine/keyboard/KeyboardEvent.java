b6b329e3cb52051d76448a73e6af105c52926b0e
Magnus <EnderCrypt@gmail.com>
2018-01-23 11:32:37 +0100

    added javadoc to KeyboardEvent

--- src/main/java/net/ddns/endertsion/gameengine/keyboard/KeyboardEvent.java
+++ src/main/java/net/ddns/endertsion/gameengine/keyboard/KeyboardEvent.java
@@ -2,6 +2,10 @@ package net.ddns.endertsion.gameengine.keyboard;
 
 import java.awt.event.KeyEvent;
 
+/**
+ * class representing a keyboard event, contain information about which key it relates to, and if shift, ctrl etc are being pressed
+ * @author EnderCrypt
+ */
 public class KeyboardEvent
 {
 	private int keyCode;
@@ -18,26 +22,46 @@ public class KeyboardEvent
 		meta = keyboardManager.keyIsHeld(KeyEvent.VK_META);
 	}
 
+	/**
+	 * returns the keycode, this can be an ASCII value or a code from {@link KeyEvent}
+	 * @return numeric KeyEvent
+	 */
 	public int getKeyCode()
 	{
 		return keyCode;
 	}
 
+	/**
+	 * returns the bind type of the event (press, hold, release)
+	 * @return bindtype
+	 */
 	public BindType getBindType()
 	{
 		return bindType;
 	}
 
+	/**
+	 * return whether or not shift is held
+	 * @return boolean
+	 */
 	public boolean isShiftHeld()
 	{
 		return shift;
 	}
 
+	/**
+	 * return whether or not ctrl is held
+	 * @return boolean
+	 */
 	public boolean isCtrlHeld()
 	{
 		return ctrl;
 	}
 
+	/**
+	 * return whether or not meta is held
+	 * @return boolean
+	 */
 	public boolean isMetaHeld()
 	{
 		return meta;
