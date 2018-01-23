0a88d3bf952b2a75120b5569e47eaf57ab06ab68
Magnus <EnderCrypt@gmail.com>
2018-01-23 13:10:26 +0100

    added javadoc for KeyboardBindFilter

--- src/main/java/net/ddns/endercrypt/library/keyboardmanager/KeyboardBindFilter.java
+++ src/main/java/net/ddns/endercrypt/library/keyboardmanager/KeyboardBindFilter.java
@@ -1,5 +1,9 @@
 package net.ddns.endercrypt.library.keyboardmanager;
 
+/**
+ * interface for creating custom keybinds
+ * @author EnderCrypt
+ */
 public interface KeyboardBindFilter
 {
 	public boolean check(KeyboardEvent keyboardEvent);
