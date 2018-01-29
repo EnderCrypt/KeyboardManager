2f3efeb6de38036d6f29ed31219af46e45c5397a
Magnus <EnderCrypt@gmail.com>
2018-01-29 20:45:55 +0100

    implemented KeyboardListenerManager.java

--- src/main/java/net/ddns/endercrypt/library/keyboardmanager/listener/KeyboardListenerManager.java
+++ src/main/java/net/ddns/endercrypt/library/keyboardmanager/listener/KeyboardListenerManager.java
@@ -1,14 +1,41 @@
 package net.ddns.endercrypt.library.keyboardmanager.listener;
 
-import net.ddns.endercrypt.library.keyboardmanager.listener.group.ListenerGroup;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.ddns.endercrypt.library.keyboardmanager.listener.group.KeyboardListenerGroup;
 
 public class KeyboardListenerManager
 {
-	private ListenerGroup globalListenerGroup = new ListenerGroup();
+	private KeyboardListenerGroup globalListenerGroup = new KeyboardListenerGroup();
+
+	private Set<KeyboardListenerGroup> listenerGroups = new HashSet<>();
 
 	public KeyboardListenerManager()
 	{
-		// TODO Auto-generated constructor stub
+		listenerGroups.add(globalListenerGroup);
 	}
 
+	public KeyboardListenerGroup getGlobalGroup()
+	{
+		return globalListenerGroup;
+	}
+
+	public boolean add(KeyboardListenerGroup listenerGroup)
+	{
+		if (listenerGroup == globalListenerGroup)
+		{
+			throw new IllegalArgumentException("Cannot add global listener");
+		}
+		return listenerGroups.add(globalListenerGroup);
+	}
+
+	public boolean remove(KeyboardListenerGroup listenerGroup)
+	{
+		if (listenerGroup == globalListenerGroup)
+		{
+			throw new IllegalArgumentException("Cannot remove global listener");
+		}
+		return listenerGroups.remove(globalListenerGroup);
+	}
 }
