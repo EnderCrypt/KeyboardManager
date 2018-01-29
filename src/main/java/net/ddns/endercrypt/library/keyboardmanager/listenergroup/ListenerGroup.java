344b7d721bd8046e39c81a8ada847e7667ba0c2f
Magnus <EnderCrypt@gmail.com>
2018-01-29 20:26:38 +0100

    created ListenerGroup package

---
+++ src/main/java/net/ddns/endercrypt/library/keyboardmanager/listenergroup/ListenerGroup.java
@@ -0,0 +1,11 @@
+package net.ddns.endercrypt.library.keyboardmanager.listenergroup;
+
+public class ListenerGroup
+{
+
+	public ListenerGroup()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
