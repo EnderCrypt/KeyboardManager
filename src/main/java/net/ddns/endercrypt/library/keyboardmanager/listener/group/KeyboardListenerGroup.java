c3191de07702dc87c88c8537f4b18b457a327a0b
Magnus <EnderCrypt@gmail.com>
2018-01-29 20:53:32 +0100

    added unimplemented methods in KeyboardListenerGroup

--- src/main/java/net/ddns/endercrypt/library/keyboardmanager/listener/group/KeyboardListenerGroup.java
+++ src/main/java/net/ddns/endercrypt/library/keyboardmanager/listener/group/KeyboardListenerGroup.java
@@ -1,11 +1,33 @@
 package net.ddns.endercrypt.library.keyboardmanager.listener.group;
 
+import net.ddns.endercrypt.library.keyboardmanager.KeyboardBindFilter;
+import net.ddns.endercrypt.library.keyboardmanager.listener.KeyboardListener;
+
 public class KeyboardListenerGroup
 {
-
 	public KeyboardListenerGroup()
 	{
 		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * method for binding a key stroke (or multiple) to a callback listener
+	 * @param keyboardBindFilter such a <code>new SpecificKey</code> or <code>new LeftKey</code>
+	 * @param keyboardListener
+	 */
+	public void bind(KeyboardBindFilter keyboardBindFilter, KeyboardListener keyboardListener)
+	{
+		/*
+		synchronized (bindings)
+		{
+			bindings.add(new Binding(keyboardBindFilter, keyboardListener));
+		}
+		*/
+	}
+
+	public void trigger()
+	{
+
 	}
 
 }
