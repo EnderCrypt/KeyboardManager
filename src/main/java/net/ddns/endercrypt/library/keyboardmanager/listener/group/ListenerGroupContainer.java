6b4696c724075e65899802658e3d6ac900e642c8
Magnus <EnderCrypt@gmail.com>
2018-01-30 12:45:20 +0100

    added javadoc for ListenerGroupContainer

--- src/main/java/net/ddns/endercrypt/library/keyboardmanager/listener/group/ListenerGroupContainer.java
+++ src/main/java/net/ddns/endercrypt/library/keyboardmanager/listener/group/ListenerGroupContainer.java
@@ -5,7 +5,12 @@ import java.util.Optional;
 import net.ddns.endercrypt.library.keyboardmanager.KeyboardBindFilter;
 import net.ddns.endercrypt.library.keyboardmanager.KeyboardEvent;
 import net.ddns.endercrypt.library.keyboardmanager.listener.KeyboardListener;
+import net.ddns.endercrypt.library.keyboardmanager.listener.ListenerGroups;
 
+/**
+ * this class allows holding and switching out a StandardListenerGroup on the go, whitout having to remove/add anything to the {@link ListenerGroups} class
+ * @author EnderCrypt
+ */
 public class ListenerGroupContainer implements ListenerGroup
 {
 	private Optional<StandardListenerGroup> elementOptional = Optional.empty();
@@ -15,11 +20,18 @@ public class ListenerGroupContainer implements ListenerGroup
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * removes the currently active StandardListenerGroup from this container
+	 */
 	public void removeListenerGroup()
 	{
 		elementOptional = Optional.empty();
 	}
 
+	/**
+	 * sets the currently active StandardListenerGroup to be triggered from keybinds
+	 * @param a StandardListenerGroup (if null, effectivly uses {@link removeListenerGroup()} )
+	 */
 	public void setListenerGroup(StandardListenerGroup element)
 	{
 		elementOptional = Optional.ofNullable(element);
